2a62ab94ce44f835ae21a07a33197de8125c4cfd
Hitesh Rana <85160299+hitesh22rana@users.noreply.github.com>
2022-06-06 15:47:03 +0530

    Time: 49 ms (84.74%), Space: 14.7 MB (31.40%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -7,55 +7,20 @@
  * };
  */
 class Solution {
-private:
-    ListNode* getnewNode(ListNode *head , int len) {
-        while(len) {
-            head = head->next;
-            len--;
-        }
-        return head;
-    }
-    
-    int getLength(ListNode* node) {
-        ListNode* temp = node;
-        
-        int cnt = 0;
-        
-        while(temp != NULL) {
-            cnt++;
-            temp = temp->next;
-        }
-        
-        return cnt;
-        
-    }
-    
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        int lengthA = getLength(headA);
-        int lengthB = getLength(headB);
-        ListNode* head;
-        
-        
-        if(lengthA > lengthB) {
-            headA = getnewNode(headA , lengthA - lengthB);
+        if(headA == NULL || headB == NULL) return NULL;
+    
+        ListNode* a = headA;
+        ListNode* b = headB;
+
+        //if a & b have different len, then we will stop the loop after second iteration
+        while(a != b){
+            //for the end of first iteration, we just reset the pointer to the head of another linkedlist
+            a = a == NULL? headB : a->next;
+            b = b == NULL? headA : b->next;    
         }
-        
-        else {
-            headB = getnewNode(headB , lengthB - lengthA);
-        }
-        
-        while(headA != NULL && headB != NULL) {
-            if(headA == headB) {
-                head = headA;
-                return head;
-            }
-            
-            headA = headA->next;
-            headB = headB->next;
-            
-        }
-        
-        return NULL;   
+
+        return a;
     }
 };
