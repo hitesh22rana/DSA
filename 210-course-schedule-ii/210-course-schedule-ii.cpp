13489c546173df3157617a7280f1638a477f3578
Hitesh Rana <85160299+hitesh22rana@users.noreply.github.com>
2022-06-23 23:42:26 +0530

    Time: 36 ms (39.02%), Space: 13.3 MB (88.52%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cpp
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -1,69 +1,49 @@
 class Solution {
 private:
-    bool checkForCycle(int src, unordered_map<int,bool>& visited, unordered_map<int,bool>& dfsVisited, vector<vector<int>>& adjList) {
-    visited[src] = true;
-    dfsVisited[src] = true;
-    
-    for(auto neighbour : adjList[src]) {
-        if(!visited[neighbour]) {
-            bool cycleFound = checkForCycle(neighbour,visited,dfsVisited,adjList);
-            if(cycleFound) return true;
-        }
-        
-        else if(dfsVisited[neighbour]) return true;
-    }
-    dfsVisited[src] = false;
-    return false;
-}
-    
-    bool isCycle(int n, vector<vector<int>>& graph) {
-        unordered_map<int,bool> visited;
-        unordered_map<int,bool> dfsVisited;
-        
-        for(int i = 0 ; i<n ; i++) {
-            if(!visited[i]) {
-                bool cycleFound = checkForCycle(i,visited,dfsVisited,graph);
-                if(cycleFound) return true;
-            }
-        }
-        return false;
-    }    
-    
-    vector<vector<int>> makeGraph(int numCourses, vector<vector<int>>& list) {
+    vector<vector<int>> makeGraph(int numCourses, vector<vector<int>>& list, vector<int>& indegree) {
         vector<vector<int>> graph(numCourses);
         
-        for(vector<int> edge : list) {
-            graph[edge[0]].push_back(edge[1]);
+        for(int i = 0 ; i<list.size() ; i++) {
+            graph[list[i][1]].push_back(list[i][0]);
+            indegree[list[i][0]]++;
         }
-        
+         
         return graph;
     }
     
-    void topologicalSort(int edge, vector<bool>& visited, vector<vector<int>>& graph , vector<int>& ans) {         
-        visited[edge] = true;
-        
-        for(int children : graph[edge]) {
-            if(!visited[children]) {
-                topologicalSort(children,visited,graph,ans);   
-            }
-        }
-        ans.push_back(edge);
-    }
 public:
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> ans;
-        vector<bool> visited(numCourses , false);
-        
-        vector<vector<int>> graph = makeGraph(numCourses, prerequisites);
-        
-        if(isCycle(numCourses, graph)) return {};
-        
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {            
+        vector<int> indegree(numCourses,0);
+        vector<vector<int>> graph = makeGraph(numCourses, prerequisites, indegree);
+
+        queue<int> q;
         for(int i = 0 ; i<numCourses ; i++) {
-            if(!visited[i]) {
-                topologicalSort(i,visited,graph,ans);
+            if(indegree[i] == 0) {
+                q.push(i);
             }
         }
         
-        return ans;
+        int count = 0;
+        vector<int> ans;
+        
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+            
+            for(int val : graph[node]) {
+                indegree[val]--;
+                if(indegree[val] == 0) {
+                    q.push(val);
+                }
+            }
+            
+            count++;
+            ans.push_back(node);
+            
+        }
+        
+        if(numCourses != count) return {};
+        
+        return ans;        
     }
 };
