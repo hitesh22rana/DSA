7f315de9693b2a98a091451ddb82f6f6001bfb3d
Hitesh Rana <85160299+hitesh22rana@users.noreply.github.com>
2022-06-24 13:52:42 +0530

    Time: 11 ms (56.30%), Space: 9.9 MB (83.01%) - LeetHub

---
+++ 2293-min-max-game/2293-min-max-game.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minMaxGame(vector<int>& nums) {
+        
+        while(nums.size() > 1) {
+            vector<int> newNums(nums.size()/2,0);
+            
+            for(int i = 0 ; i<nums.size()/2 ; i++) {
+                if(i&1) {
+                    newNums[i] = max(nums[2*i],nums[2*i+1]);
+                } else {
+                    newNums[i] = min(nums[2*i],nums[2*i+1]);
+                }
+            }
+            
+            nums = newNums;
+        }
+        
+        return nums[0];
+    }
+};
