4ce7e4141f8b31d9437e9d8378732c291a303f26
Hitesh Rana <85160299+hitesh22rana@users.noreply.github.com>
2022-07-16 10:34:40 +0530

    Time: 25 ms (30.08%), Space: 11.5 MB (12.62%) - LeetHub

--- 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int mod = pow(10,9) + 7;
     
-    int helperFunction(int row, int col, int maxMoves, int x, int y,  int moves, vector<vector<vector<int>>> & dp){
+    long long int helperFunction(int row, int col, int maxMoves, int x, int y,  int moves, vector<vector<vector<long long int>>> & dp){
         if(x < 0 || y < 0 || x >= row || y >= col){
             return 1;
         }
@@ -12,17 +12,12 @@ public:
         if(dp[moves][x][y] != -1){
             return dp[moves][x][y] % mod;
         }
-        int tempVal = 0;
-        tempVal = (tempVal + helperFunction(row, col, maxMoves, x, y - 1, moves + 1, dp)) % mod;
-        tempVal = (tempVal + helperFunction(row, col, maxMoves, x - 1, y, moves + 1, dp)) % mod;
-        tempVal = (tempVal + helperFunction(row, col, maxMoves, x + 1, y, moves + 1, dp)) % mod;
-        tempVal = (tempVal + helperFunction(row, col, maxMoves, x, y + 1, moves + 1, dp)) % mod;
-        dp[moves][x][y] = tempVal;
-        return dp[moves][x][y] % mod;
+
+        return dp[moves][x][y] = ((helperFunction(row, col, maxMoves, x, y - 1, moves + 1, dp) % mod) + (helperFunction(row, col, maxMoves, x - 1, y, moves + 1, dp) % mod) + (helperFunction(row, col, maxMoves, x + 1, y, moves + 1, dp) % mod) + (helperFunction(row, col, maxMoves, x, y + 1, moves + 1, dp) % mod)) % mod;
     }
     
     int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
-        vector<vector<vector<int>>> dp(maxMove + 1, vector<vector<int>> (m + 1, vector<int> (n + 1, -1)));
+        vector<vector<vector<long long int>>> dp(maxMove + 1, vector<vector<long long int>> (m + 1, vector<long long int> (n + 1, -1)));
         dp[maxMove][m][n] = helperFunction(m, n, maxMove, startRow, startColumn, 0, dp);
         return dp[maxMove][m][n];
     }
