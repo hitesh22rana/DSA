1c047657946db9e7de5e5094a3854669532a7cb0
Hitesh Rana <85160299+hitesh22rana@users.noreply.github.com>
2022-07-17 10:26:01 +0530

    Time: 2153 ms (11.11%), Space: 590.3 MB (11.11%) - LeetHub

--- 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
+++ 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
@@ -1,21 +1,55 @@
-class Solution {
-    public int[] smallestTrimmedNumbers(String[] nums, int[][] queries) {
-        int[] ans=new int[queries.length];
-       
-        for(int i=0;i<queries.length;i++){
+class Data {
+public:
+    int index;
+    string val;
+    
+    Data(int index, string val) {
+        this->index = index;
+        this->val = val;
+    }
+};
 
-            ArrayList<Pair<String,Integer>> hm=new ArrayList<>();
-            int ind=queries[i][1];
-            for(int j=0;j<nums.length;j++){
-                String str=nums[j];
-                String temp=str.substring(nums[j].length()-ind,nums[j].length());
-                hm.add(new Pair(temp.trim(),j));
+class compare {
+public:
+    bool operator()(Data &a, Data &b) {
+        if(a.val == b.val) return a.index < b.index;
+        return a.val < b.val;
+    }
+};
+
+class Solution {
+public:
+    vector<int> smallestTrimmedNumbers(vector<string>& nums, vector<vector<int>>& queries) {
+        vector<int> ans;
+        int n = nums.size();
+        
+        for(vector<int>& query : queries) {
+            int trimSize = query[1];
+            int k = query[0];
+            
+            priority_queue<Data,vector<Data>,compare> pq;
+            
+            for(int i = 0 ; i<n ; i++) {
+                string val = nums[i];
+                int size = val.size() - trimSize;
+                string num = val.substr(size,val.size());
+                
+                if(pq.size() >= k) {
+                    if(pq.top().val > num) {
+                        pq.pop();
+                        Data value(i,num);
+                        pq.push(value);
+                    }
+                } else {
+                    Data value(i,num);
+                    pq.push(value);
+                }
                 
             }
             
-            Collections.sort(hm,(a,b)->a.getKey().compareTo(b.getKey()));
-            ans[i]=hm.get(queries[i][0]-1).getValue();
+            ans.push_back(pq.top().index);
         }
+        
         return ans;
     }
-}
+};
