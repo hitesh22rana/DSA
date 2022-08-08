81b1d827f5f0a25b24c606eb073c100310829f3a
Hitesh Rana <85160299+hitesh22rana@users.noreply.github.com>
2022-08-08 16:34:11 +0530

    Time: 390 ms (53.22%), Space: 10.6 MB (47.88%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -4,6 +4,7 @@ public:
         vector<int> dp(nums.size(),0);
         
         dp[0] = 1;
+        int ans = 1;
         
         for(int i = 1 ; i<nums.size() ; i++) {
             int maxi = 0;
@@ -13,14 +14,9 @@ public:
                 }
             }
             dp[i] = maxi+1;
+            if(dp[i] > ans) ans = dp[i];
         }
-        
-        int ans = 0;
-        
-        for(int& val : dp) {
-            ans = max(ans,val);
-        }
-        
+
         return ans;
     }
 };
