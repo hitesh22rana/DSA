7395f657a850421f87e6cb57e52a183f8f18d4f1
Hitesh Rana <85160299+hitesh22rana@users.noreply.github.com>
2022-08-18 10:55:40 +0530

    Time: 844 ms (77.47%), Space: 170.3 MB (7.03%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int numberOfWeakCharacters(vector<vector<int>>& properties) {
-        unordered_map<int,vector<int>> mp;
+        unordered_map<int,priority_queue<int>> mp;
         int minAttack = INT_MAX;
         int maxAttack = 0;
         
         for(vector<int>& property : properties) {
             minAttack = min(minAttack,property[0]);
             maxAttack = max(maxAttack,property[0]);
-            mp[property[0]].push_back(property[1]);
+            mp[property[0]].push(property[1]);
         }
         
         int count = 0;
@@ -17,14 +17,14 @@ public:
         for(int i = maxAttack ; i>=minAttack ; i--) {
             if(mp.find(i) == mp.end()) continue;
             
-            vector<int>& arr = mp[i];    
-            int maxi = arr[0];
+            priority_queue<int>& pq = mp[i];    
+            int maxi = pq.top();
             
-            for(int& val : arr) {
-                maxi = max(val,maxi);
-                if(val < maxDefence) count++;
+            while(!pq.empty() && pq.top() >= maxDefence) {
+                pq.pop();
             }
             
+            count += pq.size();
             maxDefence = max(maxi,maxDefence);
         }        
         
