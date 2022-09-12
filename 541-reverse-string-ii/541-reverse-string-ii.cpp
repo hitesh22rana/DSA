4a90e95e084a0774e4149005e2be4f3b5add029a
Hitesh Rana <85160299+hitesh22rana@users.noreply.github.com>
2022-09-12 17:24:41 +0530

    Time: 13 ms (11.76%), Space: 8.2 MB (6.47%) - LeetHub

---
+++ 541-reverse-string-ii/541-reverse-string-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        if(s.size() < k) {
+            reverse(s.begin(),s.end());
+            return s;
+        }
+        
+        string ans;
+        string rev;
+        
+        int i = 0;
+        
+        while(i < s.size()) {
+            rev += s[i];
+            i++;
+            
+            if(rev.size() == k) {
+                reverse(rev.begin(),rev.end());
+                ans += rev;
+                rev.erase();
+
+                int j = i+k;
+                
+                while(i < s.size() && i < j) {
+                    ans.push_back(s[i]);
+                    i++;
+                }                
+            }
+        }
+        
+        if(rev.size() != k) {
+            reverse(rev.begin(),rev.end());
+            ans += rev;
+        }
+        
+        return ans;
+    }
+};
