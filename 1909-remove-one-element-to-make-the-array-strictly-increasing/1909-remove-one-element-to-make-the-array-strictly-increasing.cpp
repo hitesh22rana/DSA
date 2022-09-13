35e991ec1c333a1f1412b00a2a66defad06156b6
Hitesh Rana <85160299+hitesh22rana@users.noreply.github.com>
2022-09-13 19:54:15 +0530

    Time: 5 ms (77.39%), Space: 10 MB (62.28%) - LeetHub

--- 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.cpp
+++ 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.cpp
@@ -1,54 +1,14 @@
 class Solution {
 public:
     bool canBeIncreasing(vector<int>& nums) {
-        int count = 0;
-        
-        for(int i = 0 ; i<nums.size() ; i++) {
-            bool flag = false;
-            
-            if(i == 0) {
-                for(int j = 1 ; j<nums.size() - 1 ; j++) {
-                    if(nums[j] >= nums[j+1]) {
-                        flag = true;
-                        break;
-                    }
-                } 
+        int cnt = 0;
+        for (int i = 1; i < nums.size() && cnt < 2; ++i) {
+            if (nums[i - 1] >= nums[i]) {
+                ++cnt;
+                if (i > 1 && nums[i - 2] >= nums[i])
+                    nums[i] = nums[i - 1];
             }
-            
-            else if(i == nums.size() - 1) {
-                for(int j = 0 ; j<nums.size() - 2 ; j++) {
-                    if(nums[j] >= nums[j+1]) {
-                        flag = true;
-                        break;
-                    }
-                }
-            }
-            
-            else {
-                for(int j = 0 ; j<nums.size() - 1 ; j++) {
-                    if(i == j) continue;
-                    
-                    else if(j == i-1) {
-                        if(j+2 < nums.size() && nums[j] >= nums[j+2]) {
-                            flag = true;
-                            break;
-                        }
-                    } else {
-                        if(nums[j] >= nums[j+1]) {
-                            flag = true;
-                            break;
-                        }
-                    }
-                    
-                }    
-            }
-            
-            if(!flag) return true;
-            
-            if(flag) count++;
-                    
         }
-        
-        return count < 2;
+        return cnt < 2;
     }
 };
