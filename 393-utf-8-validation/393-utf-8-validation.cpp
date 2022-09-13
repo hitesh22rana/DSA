532ecd45ad9a1ac734cb42ffc47392994bd8ce83
Hitesh Rana <85160299+hitesh22rana@users.noreply.github.com>
2022-09-13 12:36:43 +0530

    Time: 64 ms (6.23%), Space: 16.1 MB (5.74%) - LeetHub

--- 393-utf-8-validation/393-utf-8-validation.cpp
+++ 393-utf-8-validation/393-utf-8-validation.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
-    string bitConverter(int data) {
-        string ans;
-        
+    vector<bool> bitConverter(int data) {
+        vector<bool> ans(8);
         for(int i = 0 ; i<8 ; i++) {
-            int val = data % 2;
-            ans = to_string(val) + ans;
+            ans[8-i-1] = data&1;
             data >>= 1;
         }
         
         return ans;
     }
     
-    int type(string& bitMap) {
+    int type(vector<bool>& bitMap) {
         int count = 0;
         for(int i = 0 ; i<bitMap.size() ; i++) {
-            if(bitMap[i] == '1') count++;
+            if(bitMap[i]) count++;
             else break;
         }
-        
+
         return count;
     }
     
     bool validUtf8(vector<int>& data) {
         for(int i = 0 ; i<data.size() ; i++) {
-            string bitMap = bitConverter(data[i]);
+            vector<bool> bitMap = bitConverter(data[i]);
 
             int val = type(bitMap);
             int j = i+1;
@@ -32,7 +30,7 @@ public:
             if(val == 1 || val > 4 || val > data.size() - i) return false;
             
             while(j < data.size() && val > 1) {
-                string bits = bitConverter(data[j]);
+                vector<bool> bits = bitConverter(data[j]);
                 if(type(bits) != 1) return false;
                 j++;
                 val--;
