8c7a9dc7e6ad04c8739ab9ea8bc10487f39228b3
Hitesh Rana <85160299+hitesh22rana@users.noreply.github.com>
2022-09-16 13:47:48 +0530

    Time: 673 ms (54.48%), Space: 119.9 MB (68.36%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
-    int helper(vector<int>& nums, vector<int>& multipliers, int start, int end, int pos, vector<vector<int>>& dp) {
-        if(pos == multipliers.size()) return 0;
-        
-        if(dp[pos][start] != INT_MIN) return dp[pos][start];
-        
-        int left = nums[start] * multipliers[pos] + helper(nums,multipliers,start+1,end,pos+1,dp);
-        int right = nums[end] * multipliers[pos] + helper(nums,multipliers,start,end-1,pos+1,dp);
-        
-        return dp[pos][start] = max(left,right);
-    }
-    
     int maximumScore(vector<int>& nums, vector<int>& multipliers) {
-        vector<vector<int>> dp(multipliers.size()+1,vector<int>(multipliers.size()+1,INT_MIN));
-        return helper(nums,multipliers,0,nums.size()-1,0,dp);
+        vector<vector<int>> dp(multipliers.size()+1,vector<int>(multipliers.size()+1,0));
+        
+        for(int start = multipliers.size() - 1 ; start >= 0 ; start--) {
+            for(int left = start ; left >= 0 ; left--) {
+                dp[start][left] = max(multipliers[start] * nums[left] + dp[start + 1][left + 1], multipliers[start] * nums[nums.size() - 1 - (start - left)] + dp[start + 1][left]);
+            }
+        }
+        
+        return dp[0][0];
     }
 };
