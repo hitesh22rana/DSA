fe94bc66b0daa049ee368c288563ecbf643fcb7b
Hitesh Rana <85160299+hitesh22rana@users.noreply.github.com>
2022-10-02 12:11:16 +0530

    Time: 162 ms (9.44%), Space: 10.6 MB (12.32%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     int MOD = 1e9+7;
-    int dpMem(int dice, int k, int target, vector<vector<long long int>>& dp) {
-        if(target < 0) return 0;
-        
-        if(dice == 0 && target != 0) return 0;
-        
-        if(target == 0 && dice != 0) return 0;
-        
-        if(dice == 0 && target == 0) return 1;
-        
-        if(dp[dice][target] != -1) return dp[dice][target];
-        
-        long long int ans = 0;
-        for(int i = 1 ; i<=k ; i++) {
-            ans = (ans % MOD + dpMem(dice - 1, k, target - i, dp) % MOD) % MOD;
-        }
-        
-        return dp[dice][target] = ans;
-    }
-    
+
     int numRollsToTarget(int n, int k, int target) {
-        vector<vector<long long int>> dp(n + 1, vector<long long int>(target+1,-1));
-        return dpMem(n,k,target,dp);
+        vector<vector<long long int>> dp(n + 1, vector<long long int>(target+1,0));
+        dp[0][0] = 1;
+        
+        for(int dice = 1 ; dice<=n ; dice++) {
+            for(int t = 1 ; t<=target ; t++) {
+                long long int ans = 0;
+                
+                for(int i = 1 ; i<=k ; i++) {
+                    if(t - i >= 0) {
+                        ans = ((ans % MOD) + (dp[dice - 1][t - i] % MOD)) % MOD;                    
+                    }
+                }
+                
+                dp[dice][t] = ans;
+            }
+        }
+
+        return dp[n][target];
     }
 };
